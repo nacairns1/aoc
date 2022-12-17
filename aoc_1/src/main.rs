bd5c0324ff9b9456c7d49a078acfc10ee99b07f1
= <=>
2022-12-16 21:29:05 -0500

    aoc day 1 complete

--- aoc_1/src/main.rs
+++ aoc_1/src/main.rs
@@ -1,9 +1,22 @@
-fn main() {
-    let input;
-}
+use std::error::Error;
 
-pub struct Elf {
-    food_items: Vec<Food>,
-}
+use aoc_1::*;
 
-pub struct Food(u64);
+
+
+
+fn main() -> Result<(), Box<dyn Error>> {
+    let input: String = std::fs::read_to_string("input.txt")?;
+
+    let elves_as_str = input.split('\n').collect::<Vec<&str>>();    
+    for elf_str in elves_as_str {
+
+    };
+
+    println!("{:?}", elves_as_str.get(3).unwrap());
+
+    let elf_queue = ElfQueue::from(input.to_owned());
+
+    println!("Highest Calorie Elf: {}", elf_queue.get(0).unwrap().total_calories());
+    Ok(())
+}
